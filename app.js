91ef080f2b4bc94f4370babc7488aa5e9d1c7b6e
rehansheikh000004-lab <rehansheikh000004@gmail.com>
2026-09-09 20:02:48 +0530

    Update app.js

--- app.js
+++ app.js
@@ -64,3 +64,16 @@ window.addEventListener("scroll", () => {
   const hero = document.querySelector(".hero");
   hero.style.backgroundPositionY = window.scrollY * 0.4 + "px";
 });
+
+/* WATCH CARD ANIMATION */
+
+document.querySelectorAll(".watch-card").forEach(card=>{
+  card.addEventListener("mouseenter",()=>{
+    card.style.transform="translateY(-12px) scale(1.02)";
+  });
+
+  card.addEventListener("mouseleave",()=>{
+    card.style.transform="translateY(0) scale(1)";
+  });
+});
+
